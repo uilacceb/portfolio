0e68fadb5084d9dc39fc2ba3a990a59126dbd1e5
Rebecca Liu <qu6qu6g@gmail.com>
2024-08-21 00:47:40 -0400

    nav link in tablet, desktop and mobile view done, remove the hamburger menu for now, maybe in the future will use it

--- client/src/pages/Header.jsx
+++ client/src/pages/Header.jsx
@@ -1,18 +1,19 @@
 import Nav from './Nav'
 import "./css/header.css"
-// import { Link } from "react-router-dom"
-import Menu from "../assets/menu.png"
+import { Link } from "react-router-dom"
+// import Menu from "../assets/menu.png"
 
 const Header = () => {
   return (
     <>
       <div className='header-container'>
         <div className='rebecca'>
-          <span className='rebecca-text'>Rebecca</span>
+          <Link to="/">
+            <span className='rebecca-text'>Rebecca <span className='liu-text'>Liu</span></span></Link>
         </div>
-        <div className='hbg-menu'>
+        {/* <div className='hbg-menu'>
           <img src={Menu} alt="Menu" />
-        </div>
+        </div> */}
         <Nav />
       </div>
     </>
