c1bb38cbd0d5329fee73bebce0728401fa0297fd
Rebecca Liu <qu6qu6g@gmail.com>
2024-08-25 21:39:10 -0400

    added coming soon to the project page

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -3,6 +3,9 @@
 const Projects = () => {
   return (
     <>
+      <section style={{height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+        <h1 style={{ color: '#f7c059', fontSize: '7rem',  }}>Coming Soon...</h1>
+      </section>
     </>
   )
 }
