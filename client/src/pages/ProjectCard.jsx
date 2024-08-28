6feea33c34eb49d56230c323721b8f3e1e465bca
Rebecca Liu <qu6qu6g@gmail.com>
2024-08-27 22:56:20 -0400

    added project 2

--- client/src/pages/ProjectCard.jsx
+++ client/src/pages/ProjectCard.jsx
@@ -6,16 +6,16 @@ const ProjectCard = ({ title, img, description, technologies }) => {
     <>
 
       <div className="pc-container">
-        <Link to="https://e-commerce-beige-rho.vercel.app/" target="_blank">
-          <div>
-            <h2 className="project-title">{title}</h2>
-          </div>
-          <img src={img} width={370} height={200} alt="project 1 screenshot"></img>
-          <div className="description">
-            <p><span className="tech-text">Description: </span><br></br>{description}</p>
-            <p className="tech"><span className="tech-text">Technologies</span>: {technologies}</p>
-          </div>
-        </Link>
+
+        <div className="project-title-div">
+          <h2 className="project-title">{title}</h2>
+        </div>
+        <img src={img} width={370} height={200} alt="project 1 screenshot"></img>
+        <div className="description">
+          <p><span className="tech-text">Description: </span><br></br>{description}</p>
+          <p className="tech"><span className="tech-text">Technologies</span>: {technologies}</p>
+        </div>
+
       </div>
 
     </>
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,6 +1,8 @@
 import ProjectCard from "./ProjectCard"
 import p1 from "../assets/projects/project1.png"
+import p2 from "../assets/projects/project2.png"
 import { Link } from "react-router-dom"
+import "./css/projectCard.css"
 
 
 const Projects = () => {
@@ -10,9 +12,15 @@ const Projects = () => {
       {/* <section style={{height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
         <h1 style={{ color: '#f7c059', fontSize: '7rem',  }}>Coming Soon...</h1>
       </section> */}
-      <section>
-
-        <ProjectCard title='E-commerce shopping' img={p1} description="A shopping website that allows users to add items to their cart and automatically displays the total price of all items added" technologies="React, JS, TailwindCSS" />
+      <section className="project-section">
+        {/*project 1*/}
+        <Link to="https://e-commerce-beige-rho.vercel.app/" target="_blank">
+          <ProjectCard title='E-commerce shopping' img={p1} description="A shopping website that allows users to add items to their cart and automatically displays the total price of all items added" technologies="React, JS, TailwindCSS" />
+        </Link>
+        {/* project 2 */}
+        <Link to="https://theater-app-frontend.vercel.app/" target="_blank">
+          <ProjectCard title='Theater App' img={p2} description="The Theatre App is designed to tackle the dispersed structure of the theatre industry, where details and ticket sales are frequently distributed across various platforms, websites, and physical venues." technologies="React, JS, CSS, Node.js, MongoDB" />
+        </Link>
 
       </section>
     </>
