e5642c320fae379efaa76f71d2fba8539463dab4
Rebecca Liu <qu6qu6g@gmail.com>
2024-09-03 13:55:54 -0400

    added project 4

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -2,6 +2,7 @@ import ProjectCard from "./ProjectCard";
 import p1 from "../assets/projects/project1.png";
 import p2 from "../assets/projects/project2.png";
 import p3 from "../assets/projects/project3.png";
+import p4 from "../assets/projects/project4.png";
 import { Link } from "react-router-dom";
 import "./css/projectCard.css";
 
@@ -36,6 +37,15 @@ const Projects = () => {
             title="To do list App"
             img={p3}
             description="This to-do app is perfect for anyone looking for a no-fuss way to keep track of their to-do lists. Built with a clean and intuitive design, it ensures that you can easily add new tasks, edit existing ones, and mark tasks as complete with just a few clicks."
+            technologies="React, JS, TailwindCSS"
+          />
+        </Link>
+        {/* project 4 */}
+        <Link to="https://quiz-kappa-six.vercel.app/" target="_blank">
+          <ProjectCard
+            title="Quiz App"
+            img={p4}
+            description="This Quiz App is ideal for anyone looking to test their knowledge in a fun and interactive way. Featuring a simple and user-friendly interface, it allows users to easily start a quiz, answer questions, and view their results. "
             technologies="React, JS, TailwindCSS"
           />
         </Link>
