b9b5a4bda4c075ca53b592a53075a4084ece9ef8
uilacceb <qu6qu6g@gmail.com>
2025-02-16 15:22:35 -0500

    modified projects visibility

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -4,7 +4,7 @@ import p2 from "../assets/projects/project2.png";
 //import p3 from "../assets/projects/project3.png";
 // import p4 from "../assets/projects/project4.png";
 import p5 from "../assets/projects/project5.png";
-import p6 from "../assets/projects/project6.png";
+// import p6 from "../assets/projects/project6.png";
 import p7 from "../assets/projects/project7.png";
 import p8 from "../assets/projects/project8.png";
 import p9  from "../assets/projects/project9.png";
@@ -62,25 +62,25 @@ const Projects = () => {
                 <b>P.S.</b> You can use space to increase the number.
               </>
             }
-            technologies="React, JS, CSS"
+            technologies="React, JS"
           />
         </Link>
         {/* project 6 */}
-        <Link to="https://tracking-app-beryl.vercel.app/" target="_blank">
+        {/* <Link to="https://tracking-app-beryl.vercel.app/" target="_blank">
           <ProjectCard
             title="Travel packing helper"
             img={p6}
             description="Travel Packing Helper helps users manage a list of items. Whether you're preparing for a trip or managing a to-do list, this app keeps everything organized with intuitive sorting options for packed and unpacked items."
             technologies="React, JS, CSS"
           />
-        </Link>
+        </Link> */}
         {/* project 5 */}
         <Link to="https://word-analytic.vercel.app/" target="_blank">
           <ProjectCard
             title="Word Analytics App"
             img={p5}
             description="Word Analytic is a simple tool designed to help users quickly count words and characters in any text. Whether you're working on a document, social media post, or any writing task, it allows you to input or paste your text and instantly see the word and character count."
-            technologies="React, JS, CSS"
+            technologies="React, JS"
           />
         </Link>
         {/* project 4 */}
